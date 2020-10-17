efbe3d447efd3892eeddcf879cc0133a2eeb5f2a
Rafael Santana <rafaelsantana.devweb@gmail.com>
2020-10-17 10:17:30 -0300

    starting mobile app

---
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function OrphangeDetails() {
+  return (
+    <View>
+      <Text>Heelo Detail</Text>
+    </View>
+  )
+}
